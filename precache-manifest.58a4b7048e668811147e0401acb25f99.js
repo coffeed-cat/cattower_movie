1c4ef1bb24eb25e3063fbe7211ad82521a1410a9
coffeed-cat <gwcattower@gmail.com>
2020-12-31 15:59:35 +0900

    Updates

---
+++ precache-manifest.58a4b7048e668811147e0401acb25f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa05f49027ef1ec8067b8bccb1e28b07",
+    "url": "/cattower_movie/index.html"
+  },
+  {
+    "revision": "51f329886551c8e599ba",
+    "url": "/cattower_movie/static/css/main.14a734ac.chunk.css"
+  },
+  {
+    "revision": "7977505a1e41448af17d",
+    "url": "/cattower_movie/static/js/2.efaedd63.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/cattower_movie/static/js/2.efaedd63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f329886551c8e599ba",
+    "url": "/cattower_movie/static/js/main.aa204fb4.chunk.js"
+  },
+  {
+    "revision": "dbb81c3f85e1dbb3e62f",
+    "url": "/cattower_movie/static/js/runtime-main.5a3dbcf6.js"
+  }
+]);
